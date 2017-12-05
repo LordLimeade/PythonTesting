36ed4089dafdc478324792d745c35cd94bb6f3c1
RyanB <ryan.m.blakley@gmail.com>
2017-12-05 12:26:18 -0600

    Modularize Program

--- PythonHello/PythonHello.py
+++ PythonHello/PythonHello.py
@@ -1,22 +1,46 @@
 from decimal import Decimal
 
+def getValues():
+        check = True
+
+        while (check):
+            try:
+                pay = Decimal(input("How much do you make per hour?\n" ))
+                worked = int(input("How many hours do you work each week?\n" ))
+                
+                check = False
+            except:
+                print ("Please enter a valid number")
+
+        return pay, worked
+
+def calcs(hourly, hours):
+    weekly = hourly * hours
+    salary = weekly * 52
+    return salary
+
+def output(salary):
+    print("Your Salary is ${0} dollars.".format(salary))
+
 print ("Hello Python!")
 four = 2 + 2
 print ("2 + 2 =", four)
-check = True
 
-while (check):
-    try:
-        hourly = Decimal(input("How much do you make per hour?\n" ))
-        hours = int(input("How many hours do you work each week?\n" ))
-        weekly = hourly * hours
-        salary = weekly * 52
-        print("Your Salary is ${0} dollars.".format(salary))
-        check = False
-    except:
-        print ("Please enter a valid number")
+
+hourly = 0.0
+hours = 0
+salary = 0
+
+hourly, hours = getValues()
+
+salary = calcs(hourly, hours)
+
+output(salary)
 
 print ("Have a nice day")
+
+
+
 
 
 
